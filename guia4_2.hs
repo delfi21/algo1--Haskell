872a9775ededf04f6a9dabe609d5c771898d61a8
delfisolis <164261606+delfi21@users.noreply.github.com>
2024-04-17 13:59:32 -0300

    Add files via upload

--- guia4_2.hs
+++ guia4_2.hs
@@ -21,12 +21,45 @@ sumaimpares n| mod n 2 /= 0 = 2*(n*(div (n+1) 2))-1
 sumaimpares2 :: Int -> Int 
 sumaimpares2 n | n == 0 = 0
                | n == 1 = 1
-               | otherwise = sumaimpares2(n-1)+(2*n-1)
+               | otherwise = sumaimpares2(n-1)+(2*n-1) -- aca tengo un error 
  -- ejercicio 5
- medioFact :: Int -> Int 
- medioFact n | n == 0 || n == 1 = 1
-             | otherwise = medioFact(n-2)*n 
+-- medioFact :: Int -> Int 
+ --medioFact n | n == 0 || n == 1 = 1
+             -- | otherwise = medioFact(n-2)*n 
 --ejercicio 6
 sumadigitos :: Int -> Int
 sumadigitos n | n < 10 = n
- 
+
+--ejercicio 10 
+f2 :: Int -> Float -> Float
+f2 0 _ = 0
+f2 _ 0 = 0
+f2 n q = f2 (n-1) q + q^n 
+
+f4 :: Int -> Float -> Float 
+f4 0 _ = 0
+f4 _ 0 = 0
+f4 1 q = q^1+ q^2
+f4 n q = f4 (n-1) q - q^(n-1) + q^(2*n-1)+ q^(2*n) 
+
+-- ejercicio 11
+-- defino primero la funcion factorial
+factorial :: Integer -> Integer
+factorial n | n == 0 = 1
+            |otherwise = factorial (n-1)*n
+--  definimos ahora la funcion que aproxima a e
+aprox_e :: Integer -> Float 
+aprox_e 0 = 1
+aprox_e n = aprox_e(n-1)+(1/fromIntegral(factorial(n)))
+
+-- ejercicio 12
+sucesion :: Integer -> Float
+sucesion 1 = 2
+sucesion n = 2 + (1/sucesion(n-1))
+raizde2Aprox :: Integer -> Float 
+raizde2Aprox n = sucesion(n) - 1
+
+-- ejercicio 13
+sumaDoble :: Integer -> Float -> Float
+sumaDoble m q n = sumaDoble(n-1)*m+f2(m q) 
+--algo asi 
